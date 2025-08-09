cd78c7ad73c3dbc52b15413f1eb7c96c5305c06a
Olexii Babii <babiy0617@gmail.com>
2025-08-09 09:45:48 +0300

    fix_2

--- app/main.py
+++ app/main.py
@@ -5,10 +5,6 @@ class Animal:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
-    #     self.__repr__()
-
-    # def __repr__(self) -> None:
-    #     return self.name
 
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
@@ -18,17 +14,12 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, is_hungry)
-        if is_hungry is True:
-            self.appetite = 3
-        else:
-            self.appetite = 0
+        super().__init__(name, 3, is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
@@ -36,11 +27,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, is_hungry)
-        if is_hungry is True:
-            self.appetite = 7
-        else:
-            self.appetite = 0
+        super().__init__(name, 7, is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
